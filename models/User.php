7bcc24007cef90c8d10bbba5dea94ab56ac45866
Eugene Skrobov <skrobov.ev@gmail.com>
2022-09-16 22:57:50 +0700

    Add rating to user model

--- models/User.php
+++ models/User.php
@@ -20,6 +20,7 @@ use yii\db\ActiveRecord;
  * @property string|null $telegram
  * @property string|null $description
  * @property string|null $avatar_url
+ * @property float $rating
  *
  * @property Category[] $categories
  * @property City $city
@@ -45,7 +46,7 @@ class User extends ActiveRecord
             [['name', 'email', 'password_hash', 'city_id'], 'required'],
             [['city_id', 'is_contacts_hidden'], 'integer'],
             [['role'], 'string'],
-            [['created_at', 'birthdate'], 'safe'],
+            [['created_at', 'birthdate', 'rating'], 'safe'],
             [['name', 'email', 'avatar_url'], 'string', 'max' => 255],
             [['password_hash'], 'string', 'max' => 60],
             [['phone_number'], 'string', 'max' => 11],
