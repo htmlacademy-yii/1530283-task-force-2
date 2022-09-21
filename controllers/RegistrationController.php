f49101b7eb3fcc8c53e7e2ac02b5d4cd67d29402
Eugene Skrobov <skrobov.ev@gmail.com>
2022-09-22 00:28:29 +0700

    Create RegistrationForm model

--- controllers/RegistrationController.php
+++ controllers/RegistrationController.php
@@ -2,8 +2,10 @@
 
 namespace app\controllers;
 
-use app\models\RegistrationForm;
+use Yii;
 use yii\web\Controller;
+use app\models\City;
+use app\models\RegistrationForm;
 
 class RegistrationController extends Controller
 {
@@ -16,11 +18,35 @@ class RegistrationController extends Controller
     {
         $registrationFormModel = new RegistrationForm();
 
-        // todo: Получить все города
+        if (Yii::$app->request->getIsPost()) {
+            $registrationFormModel->load(Yii::$app->request->post());
+            if ($registrationFormModel->validate()) {
+                echo 'Форма валидна';
+                //        var_dump(Yii::$app->security->passwordHashStrategy);
+//                Yii::$app->security->generatePasswordHash($user->password);
+            }
+        }
+
+        $cities = City::find()
+                      ->select('name')
+                      ->indexBy('id')
+                      ->column();
+
+
+        if (!$registrationFormModel->cityId) {
+            $defaultCityId = (int)City::find()
+                                      ->select('id')
+                                      ->one()->id;
+            $registrationFormModel->cityId = $defaultCityId;
+            $registrationFormModel->validate('cityId');
+        }
 
         return $this->render(
             'index',
-            ['registrationFormModel' => $registrationFormModel]
+            [
+                'cities' => $cities,
+                'registrationFormModel' => $registrationFormModel
+            ]
         );
     }
 }
--- models/RegistrationForm.php
+++ models/RegistrationForm.php
@@ -10,4 +10,64 @@ use yii\base\Model;
  */
 class RegistrationForm extends Model
 {
+    public string $name = '';
+    public string $email = '';
+    public string $password = '';
+    public string $repeatedPassword = '';
+    public string|int $cityId = '';
+    public bool $isContractor = true;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function formName()
+    {
+        return '';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'name' => 'Имя',
+            'email' => 'Email',
+            'password' => 'Пароль',
+            'repeatedPassword' => 'Повтор пароля',
+            'cityId' => 'Город',
+            'isContractor' => 'я собираюсь откликаться на заказы'
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules(): array
+    {
+        return [
+            [
+                [
+                    'name',
+                    'email',
+                    'password',
+                    'repeatedPassword',
+                    'cityId'
+                ],
+                'required'
+            ],
+            ['email', 'email'],
+            [['name', 'email'], 'string', 'max' => 255],
+            [['password'], 'string', 'min' => 6, 'max' => 255],
+            ['repeatedPassword', 'compare', 'compareAttribute' => 'password'],
+            ['email', 'unique', 'targetClass' => User::class],
+            [
+                'cityId',
+                'exist',
+                'targetClass' => City::class,
+                'targetAttribute' => 'id'
+            ],
+            ['isContractor', 'safe'],
+        ];
+    }
 }
